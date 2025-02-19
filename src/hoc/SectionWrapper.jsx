e43a5a8b64be9dc8f19063f6a17f697834f7958f
MetunNivin <metunnivin@gmail.com>
2025-02-19 13:03:38 -0500

    fixed mobile display issues

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -1,19 +1,21 @@
 import { motion } from "framer-motion";
-
 import { styles } from "../styles";
 import { staggerContainer } from "../utils/motion";
+import useMediaQuery from "../utils/useMediaQuery";
 
 const SectionWrapper = (Component, idName) =>
   function HOC() {
+    const isLargeScreen = useMediaQuery("(min-width: 1024px)");
+
     return (
       <motion.section
-        variants={staggerContainer()}
-        initial='hidden'
-        whileInView='show'
+        variants={isLargeScreen ? staggerContainer() : {}}
+        initial={isLargeScreen ? "hidden" : "visible"}
+        whileInView={isLargeScreen ? "show" : "visible"}
         viewport={{ once: true, amount: 0.25 }}
         className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
       >
-        <span className='hash-span' id={idName}>
+        <span className="hash-span" id={idName}>
           &nbsp;
         </span>
 
